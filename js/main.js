daf1ae4ce98de5a009d203be05a41727d3bebc1f
jacqueline <jamjacqueline@gmail.com>
2016-11-04 15:47:06 -0300

    tareas

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+
+function agregaTarea(){
+
+var tarea=document.getElementById("tarea").value;
+var boton= document.getElementById("boton");
+
+	if (tarea===null || tarea===""){
+
+		alert("Debe ingresar una tarea");
+		return false;
+	}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
