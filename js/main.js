c47a0d67924a68242044aea2ae9c429277c70d5f
jacqueline <jamjacqueline@gmail.com>
2016-11-12 19:10:48 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -16,49 +16,134 @@ var boton= document.getElementById("boton");
 
 }*/
 
-
-function agregaTarea(){
-//elementos que rescatamos
-var nombre=document.getElementById("nombre").value;
-var porhacer=document.getElementById("cajatarea").value;
-//este es el contendeor
-var contenedorPosteos=document.getElementById("contenedor-tareas");
 //crea el div que contiene la tarea
-var posteo= document.createElement("div");
+/*var posteo= document.createElement("div");
 //crea el div que contiene nuestro post.
-var elementoNombre=document.createElement("strong")
-var elementoComentario= document.createElement("p");
+//var elementoNombre=document.createElement("strong")
+var elementoTarea= document.createElement("p");
 //crea el div que contiene nuestro post.
 var elementoLinea= document.createElement("hr");
 
 //aca cremos los nodes de text
-var nodoTextoNombre=document.createTextNode(nombre);
+//var nodoTextoNombre=document.createTextNode(nombre);
 var nodoTextoComentario=document.createTextNode(porhacer);
-
+var cajatxt=document.createElement("input");
 //aca metemos los  nodos de texto a los elemntos contendeores 
-elementoNombre.appendChild(nodoTextoNombre);
-elementoComentario.appendChild(nodoTextoComentario);
+//elementoNombre.appendChild(nodoTextoNombre);
+elementoTarea.appendChild(nodoTextoComentario);
+cajatxt.appendChild(elementoTarea);
+
 
 //aca vamos a meter los tres elemetnos al div posteo
-posteo.appendChild(elementoNombre);
-posteo.appendChild(elementoComentario);
+//posteo.appendChild(elementoNombre);
+posteo.appendChild(elementoTarea);
 posteo.appendChild(elementoLinea);
+
+for (var i = 0; i <= porhacer.chidren.length-1; i++) {
+	porhacer.chidren[i].addEventListener("click", function(){
+		this.parentNode.removeChild(this);
+
+
+	});
+}
+
 
 //aca tenemos que agregar el atributo class list y el valor 'posteo'
 
-posteo.setAttribute("class", "posteo");
+//posteo.setAttribute("class", "posteo");
 
 //aca agreamos el posteo al contenedor de posteos
-contenedorPosteos.appendChild(posteo);
+//contenedorPosteos.appendChild(posteo);
 
-}
+};
 
-  function eliminarTarea (){
+  //function eliminarTarea (){
+		//this.parentNode.removeChild(this);
+	//};
+ 
+ //tareaInput.addEventListener("click", comprobarInput);
+	
+	//for (var i = 0; i <= tarea.children.length -1; i++) {
+		//tarea.children[i].addEventListener("click", eliminarTarea);
+//elementos que rescatamos
+//var nombre=document.getElementById("nombre").value;	//}*/
+//este es el contendeor
+
+
+/*	
+function agregaTarea(){
+	var porhacer=document.getElementById("cajaTarea").value;
+	var contenedorTareas=document.getElementById("contenedor-tareas");
+
+
+	var agregaTarea = function(){
+		var tarea= cajaTarea.value;	
+		var nuevaTarea=document.createElement("input");
+		var contenido=document.createTextNode(tarea);
+
+	if (tarea===""){
+
+		cajaTarea.setAttribute("placeholder", "No puede dejar el espacio vacío");
+		return false;
+	}
+	cajaTarea.value="";//  para volver al mensaje del principio añadir tarea
+
+	for (var i = 0; i < contenedor-tareas.children.length-1; i++) {
+		contenedor-tareas.children[i].addEventListener("click", function(){
+			this.parentNode.removeChild(this);
+		})
+	   }
+
+	 var eliminarTarea=function(){
 		this.parentNode.removeChild(this);
 	};
- 
- tareaInput.addEventListener("click", comprobarInput);
+}();
+*/
+
+(function(){
+	var tarea=document.getElementById("contenedor-tareas"),
+		nuevoTxt=document.getElementById("cajaTarea"),
+		btnAgregar=document.getElementById("boton");
+
+var agregaTarea= function(){
+
+	var entradaTxt = nuevoTxt.value;
+	var	contenedor=document.createElement("div"),
+		escrito=document.createTextNode(entradaTxt);//es como el ToString,
+
+if(escrito===""){
+
+	contenedor.setAttribute("placeholder", "Escriba una tarea");
+	return false;
+}
+ 	
+ 	contenedor.appendChild(escrito);//
+ 	tarea.appendChild(contenedor);
+
+ 	entradaTxt.value=""; // despues de que haga la funcion anterior
+ 							// la caja textarea quede sin el texto de lo ingresado	
+
+for (var i = 0; i <= tarea.children.length-1; i++) {
+	tarea.children[i].addEventListener("click", function(){
+		this.parentNode.removeChild(this);
+	});
+  }
+};	
+
+var comprobarInput=function(){
 	
-	for (var i = 0; i <= tarea.children.length -1; i++) {
-		tarea.children[i].addEventListener("click", eliminarTarea);
-	}
+	cajaTarea.setAttribute("placeholder", "Añada una tarea");
+};// esta ; es para cerrar la funcion definitivamente, sino seguira en un loop
+ 
+ var quitarTarea = function(){
+ 	this.parentNode.removeChild(this);
+ };
+
+ for (var i = 0; i <= tarea.children.length-1; i++) {
+ 	tarea.children[i].addEventListener("click", quitarTarea);// las variables no van entre comillas
+ }
+ //Aqui se agregan los eventos
+	btnAgregar.addEventListener("click", agregaTarea);
+	cajaTarea.addEventListener("click", comprobarInput);
+
+}())
